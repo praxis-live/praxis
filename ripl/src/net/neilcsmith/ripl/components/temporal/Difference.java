0ca79e226bc0dca124a75e5b06253abf8d139371
Neil C Smith <neilcsmith.net@googlemail.com>
2012-04-13 15:43:08 +0100

    Switch to DifferenceOp.

--- ripl/src/net/neilcsmith/ripl/components/temporal/Difference.java
+++ ripl/src/net/neilcsmith/ripl/components/temporal/Difference.java
@@ -27,7 +27,6 @@ import net.neilcsmith.ripl.SurfaceCapabilities;
 //import net.neilcsmith.ripl.core.SurfaceOp;
 import net.neilcsmith.ripl.impl.SingleInOut;
 import net.neilcsmith.ripl.ops.DifferenceOp;
-import static net.neilcsmith.ripl.rgbmath.RGBMath.*;
 
 /**
  *
@@ -101,205 +100,14 @@ public class Difference extends SingleInOut {
                 surface.process(op, lastFrame);
                 surface.clear(); // output will be garbage
             } else {
+                Surface tmp = lastFrame.createSurface(null);
+                tmp.copy(surface);
                 surface.process(op, lastFrame);
+                lastFrame.copy(tmp);
+                tmp.release();
             }
         }
     }
 
-//    public static class Op implements SurfaceOp {
-//
-//        private double threshold;
-//        private Mode mode;
-//
-//        public Op() {
-//            this.threshold = 0;
-//            this.mode = Mode.Color;
-//        }
-//
-//        public void process(PixelData output, PixelData... inputs) {
-//            if (inputs.length < 1) {
-//                return;
-//            }
-//            switch (mode) {
-//                case Color:
-//                    processColor(output, inputs[0]);
-//                    break;
-//                case Mono:
-//                    processMono(output, inputs[0]);
-//                    break;
-//                case Threshold:
-//                    processThreshold(output, inputs[0]);
-//                    break;
-//            }
-//
-//        }
-//
-//        public void setThreshold(double threshold) {
-//            if (threshold < 0 || threshold > 1) {
-//                throw new IllegalArgumentException();
-//            }
-//
-//            this.threshold = threshold;
-//        }
-//
-//        public double getThreshold() {
-//            return threshold;
-//        }
-//
-//        public void setMode(Mode mode) {
-//            if (mode == null) {
-//                throw new NullPointerException();
-//            }
-//            this.mode = mode;
-//        }
-//
-//        public Mode getMode() {
-//            return mode;
-//        }
-//
-//        private void processColor(PixelData output, PixelData input) {
-//            int thres = (int) Math.round(threshold * 256);
-//            int width = Math.min(input.getWidth(), output.getWidth());
-//            int height = Math.min(input.getHeight(), output.getHeight());
-//            int bgDelta = input.getScanline() - width;
-//            int fgDelta = output.getScanline() - width;
-//            int[] bgData = input.getData();
-//            int[] fgData = output.getData();
-//            int bgIdx = 0;
-//            int fgIdx = 0;
-//
-//            int fg, fgR, fgG, fgB;
-//            int bg, bgR, bgG, bgB;
-//            int maxDelta;
-//
-//            for (int y = 0; y < height; y++) {
-//                for (int x = 0; x < width; x++) {
-//                    fg = fgData[fgIdx];
-//                    fgR = (fg & RED_MASK) >>> 16;
-//                    fgG = (fg & GREEN_MASK) >>> 8;
-//                    fgB = fg & BLUE_MASK;
-//                    bg = bgData[bgIdx];
-//                    bgR = (bg & RED_MASK) >>> 16;
-//                    bgG = (bg & GREEN_MASK) >>> 8;
-//                    bgB = bg & BLUE_MASK;
-//
-//                    fgR = diff(fgR, bgR);
-//                    fgG = diff(fgG, bgG);
-//                    fgB = diff(fgB, bgB);
-//
-//                    maxDelta = max(fgR, fgG, fgB);
-//                    if (maxDelta < thres) {
-//                        fgData[fgIdx] = 0;
-//                    } else {
-//                        fgData[fgIdx] = fg & ALPHA_MASK |
-//                                (fgR << 16) | (fgG << 8) | fgB;
-//                    }
-//                    bgData[bgIdx] = fg;
-//
-//                    bgIdx++;
-//                    fgIdx++;
-//                }
-//                bgIdx += bgDelta;
-//                fgIdx += fgDelta;
-//            }
-//
-//        }
-//
-//        private void processMono(PixelData output, PixelData input) {
-//            int thres = (int) Math.round(threshold * 256);
-//            int width = Math.min(input.getWidth(), output.getWidth());
-//            int height = Math.min(input.getHeight(), output.getHeight());
-//            int bgDelta = input.getScanline() - width;
-//            int fgDelta = output.getScanline() - width;
-//            int[] bgData = input.getData();
-//            int[] fgData = output.getData();
-//            int bgIdx = 0;
-//            int fgIdx = 0;
-//
-//            int fg, fgR, fgG, fgB;
-//            int bg, bgR, bgG, bgB;
-//            int maxDelta;
-//
-//            for (int y = 0; y < height; y++) {
-//                for (int x = 0; x < width; x++) {
-//                    fg = fgData[fgIdx];
-//                    fgR = (fg & RED_MASK) >>> 16;
-//                    fgG = (fg & GREEN_MASK) >>> 8;
-//                    fgB = fg & BLUE_MASK;
-//                    bg = bgData[bgIdx];
-//                    bgR = (bg & RED_MASK) >>> 16;
-//                    bgG = (bg & GREEN_MASK) >>> 8;
-//                    bgB = bg & BLUE_MASK;
-//
-//                    fgR = diff(fgR, bgR);
-//                    fgG = diff(fgG, bgG);
-//                    fgB = diff(fgB, bgB);
-//
-//                    maxDelta = max(fgR, fgG, fgB);
-//                    if (maxDelta < thres) {
-//                        fgData[fgIdx] = 0;
-//                    } else {
-//                        fgData[fgIdx] = fg & ALPHA_MASK |
-//                                (maxDelta << 16) | (maxDelta << 8) | maxDelta;
-//                    }
-//                    bgData[bgIdx] = fg;
-//
-//                    bgIdx++;
-//                    fgIdx++;
-//                }
-//                bgIdx += bgDelta;
-//                fgIdx += fgDelta;
-//            }
-//
-//        }
-//
-//        private void processThreshold(PixelData output, PixelData input) {
-//            int thres = (int) Math.round(threshold * 256);
-//            int width = Math.min(input.getWidth(), output.getWidth());
-//            int height = Math.min(input.getHeight(), output.getHeight());
-//            int bgDelta = input.getScanline() - width;
-//            int fgDelta = output.getScanline() - width;
-//            int[] bgData = input.getData();
-//            int[] fgData = output.getData();
-//            int bgIdx = 0;
-//            int fgIdx = 0;
-//
-//            int fg, fgR, fgG, fgB;
-//            int bg, bgR, bgG, bgB;
-//            int maxDelta;
-//
-//            boolean alpha = output.hasAlpha();
-//
-//            for (int y = 0; y < height; y++) {
-//                for (int x = 0; x < width; x++) {
-//                    fg = fgData[fgIdx];
-//                    fgR = (fg & RED_MASK) >>> 16;
-//                    fgG = (fg & GREEN_MASK) >>> 8;
-//                    fgB = fg & BLUE_MASK;
-//                    bg = bgData[bgIdx];
-//                    bgR = (bg & RED_MASK) >>> 16;
-//                    bgG = (bg & GREEN_MASK) >>> 8;
-//                    bgB = bg & BLUE_MASK;
-//
-//                    fgR = diff(fgR, bgR);
-//                    fgG = diff(fgG, bgG);
-//                    fgB = diff(fgB, bgB);
-//
-//                    maxDelta = max(fgR, fgG, fgB);
-//                    if (maxDelta < thres) {
-//                        fgData[fgIdx] = 0;
-//                    } else {
-//                        fgData[fgIdx] = alpha ? 0xFFFFFFFF : 0xFFFFFF;
-//                    }
-//                    bgData[bgIdx] = fg;
-//
-//                    bgIdx++;
-//                    fgIdx++;
-//                }
-//                bgIdx += bgDelta;
-//                fgIdx += fgDelta;
-//            }
-//
-//        }
-//    }
+
 }
