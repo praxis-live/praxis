2a42b9256cf29736f331804a7528594cd9514e53
Neil C Smith <neilcsmith.net@googlemail.com>
2012-06-13 19:01:19 +0100

    praxis.video-opengl - add video:opengl:filter component, fix up factories.
    --HG--
    rename : praxis.video-java/src/META-INF/services/net.neilcsmith.praxis.core.ComponentFactoryProvider => praxis.video-opengl/src/META-INF/services/net.neilcsmith.praxis.core.ComponentFactoryProvider
    rename : praxis.video-java/src/net/neilcsmith/praxis/video/java/components/JavaVideoFactoryProvider.java => praxis.video-opengl/src/net/neilcsmith/praxis/video/opengl/GLFactoryProvider.java
    rename : praxis.video-opengl/src/net/neilcsmith/praxis/video/opengl/GLFactoryProvider.java => praxis.video-opengl/src/net/neilcsmith/praxis/video/opengl/GLPlayerFactoryProvider.java

--- praxis.video-opengl/src/net/neilcsmith/praxis/video/opengl/GLFactoryProvider.java
+++ praxis.video-opengl/src/net/neilcsmith/praxis/video/opengl/GLFactoryProvider.java
@@ -1,68 +1,39 @@
 /*
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
- * Copyright 2011 Neil C Smith.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 3 only, as
- * published by the Free Software Foundation.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 3 for more details.
- *
- * You should have received a copy of the GNU General Public License version 3
- * along with this work; if not, see http://www.gnu.org/licenses/
- * 
- *
- * Please visit http://neilcsmith.net if you need additional information or
- * have any questions.
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
  */
+
 package net.neilcsmith.praxis.video.opengl;
 
-import net.neilcsmith.praxis.video.ClientConfiguration;
-import net.neilcsmith.praxis.video.PlayerConfiguration;
-import net.neilcsmith.praxis.video.PlayerFactory;
-import net.neilcsmith.ripl.Player;
-import net.neilcsmith.ripl.render.opengl.GLPlayer;
+import net.neilcsmith.praxis.core.ComponentFactory;
+import net.neilcsmith.praxis.core.ComponentFactoryProvider;
+import net.neilcsmith.praxis.impl.AbstractComponentFactory;
 
 /**
  *
- * @author nsigma
+ * @author Neil C Smith (http://neilcsmith.net)
  */
-public class GLFactoryProvider implements PlayerFactory.Provider {
-    
-    private final static String LIBRARY_NAME = "OpenGL";
-    private final static PlayerFactory factory = new Factory(); 
+public class GLFactoryProvider implements ComponentFactoryProvider {
 
-    @Override
-    public PlayerFactory getFactory() {
-        return factory;
+
+    private static Factory instance = new Factory();
+
+    public ComponentFactory getFactory() {
+        return instance;
     }
 
-    @Override
-    public String getLibraryName() {
-        return LIBRARY_NAME;
-    }
-    
-    private static class Factory implements PlayerFactory {
+    private static class Factory extends AbstractComponentFactory {
 
-        @Override
-        public Player createPlayer(PlayerConfiguration config, ClientConfiguration[] clients)
-            throws Exception {
-            if (clients.length != 1 || clients[0].getSourceCount() != 0 || clients[0].getSinkCount() != 1) {
-                throw new IllegalArgumentException("Invalid client configuration");
-            }
-            Object fs = clients[0].getHint(ClientConfiguration.CLIENT_KEY_FULLSCREEN);
-            boolean fullscreen = false;
-            if (fs instanceof Boolean) {
-                fullscreen = ((Boolean)fs).booleanValue();
-            }
-            return GLPlayer.create("OPENGL TEST", config.getWidth(), config.getHeight(), config.getFPS(), fullscreen);
-            
+        private Factory() {
+            build();
         }
-        
+
+        private void build() {      
+
+            addComponent("video:opengl:filter", GLFilter.class);
+
+        }
+
     }
-    
+
 }
