edf41cbccbd1470ee07b55e64ff6027fe2970af5
Neil C Smith <neilcsmith.net@googlemail.com>
2013-02-07 12:07:55 +0000

    praxis.live.video - Add missing copyright notice on Flob port.

--- praxis.video/src/net/neilcsmith/praxis/video/components/analysis/blobs/BaseBlob.java
+++ praxis.video/src/net/neilcsmith/praxis/video/components/analysis/blobs/BaseBlob.java
@@ -1,3 +1,27 @@
+/*
+ * Code ported from flob - http://s373.net/code/flob/
+ *
+ * flob // flood-fill multi-blob detector
+ *
+ * (c) copyright 2008-2010 andré sier
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA  02111-1307  USA
+ */
+
+
 package net.neilcsmith.praxis.video.components.analysis.blobs;
 
 public abstract class BaseBlob {
