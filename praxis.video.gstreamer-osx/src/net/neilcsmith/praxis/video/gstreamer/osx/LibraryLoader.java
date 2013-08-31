741bfba463040f44cce8263372191b7f842c026a
Neil C Smith <neilcsmith.net@googlemail.com>
2013-08-31 16:21:51 +0100

    praxis.video.gstreamer-osx - first commit of GStreamer lib module for OSX.
    --HG--
    rename : praxis.video.gstreamer-win/src/META-INF/services/net.neilcsmith.praxis.video.gstreamer.GStreamerLibraryLoader => praxis.video.gstreamer-osx/src/META-INF/services/net.neilcsmith.praxis.video.gstreamer.GStreamerLibraryLoader
    rename : praxis.video.gstreamer-win/src/net/neilcsmith/praxis/video/gstreamer/windows/LibraryLoader.java => praxis.video.gstreamer-osx/src/net/neilcsmith/praxis/video/gstreamer/osx/LibraryLoader.java
    rename : praxis.video.gstreamer-win/src/net/neilcsmith/praxis/video/gstreamer/windows/LibraryLoaderImpl.java => praxis.video.gstreamer-osx/src/net/neilcsmith/praxis/video/gstreamer/osx/LibraryLoaderImpl.java

---
+++ praxis.video.gstreamer-osx/src/net/neilcsmith/praxis/video/gstreamer/osx/LibraryLoader.java
@@ -0,0 +1,45 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright 2012 Neil C Smith.
+ * 
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3 only, as
+ * published by the Free Software Foundation.
+ * 
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 3 for more details.
+ * 
+ * You should have received a copy of the GNU General Public License version 3
+ * along with this work; if not, see http://www.gnu.org/licenses/
+ * 
+ * 
+ * Please visit http://neilcsmith.net if you need additional information or
+ * have any questions.
+ */
+package net.neilcsmith.praxis.video.gstreamer.osx;
+
+import com.sun.jna.Platform;
+import java.util.logging.Logger;
+import net.neilcsmith.praxis.video.gstreamer.GStreamerLibraryLoader;
+
+/**
+ *
+ * @author Neil C Smith (http://neilcsmith.net)
+ */
+public class LibraryLoader implements GStreamerLibraryLoader {
+    
+    static final Logger LOG = Logger.getLogger(LibraryLoader.class.getName());
+
+    @Override
+    public void load() throws Exception {
+        if (!Platform.isMac()) {
+            LOG.fine("Windows GStreamer Loader ignoring - platform isn't Windows.");
+            return;
+        }
+        LibraryLoaderImpl.getInstance().load();
+    }
+    
+}
