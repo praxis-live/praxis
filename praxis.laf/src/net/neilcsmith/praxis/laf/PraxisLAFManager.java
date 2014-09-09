04cd2bd0166e8b7968eccdc6a3a6123b7db64cb1
Neil C Smith <neilcsmith.net@googlemail.com>
2014-09-09 15:04:00 +0100

    praxis.laf - replace old look and feel in core suite with subset of new Praxis LIVE look and feel.

--- praxis.laf/src/net/neilcsmith/praxis/laf/PraxisLAFManager.java
+++ praxis.laf/src/net/neilcsmith/praxis/laf/PraxisLAFManager.java
@@ -1,30 +1,27 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 2010 Neil C Smith.
+ * Copyright 2014 Neil C Smith.
  *
  * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation; either version 2.1 of the License,
- * or (at your option) any later version.
+ * under the terms of the GNU General Public License version 3 only, as
+ * published by the Free Software Foundation.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
- * for more details.
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 3 for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
+ * You should have received a copy of the GNU General Public License version 3
  * along with this work; if not, see http://www.gnu.org/licenses/
- * 
+ *
  *
  * Please visit http://neilcsmith.net if you need additional information or
  * have any questions.
- *
  */
 
 package net.neilcsmith.praxis.laf;
 
-import com.nilo.plaf.nimrod.NimRODLookAndFeel;
 import java.awt.EventQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -70,8 +67,6 @@ public class PraxisLAFManager {
             return;
         }
         try {
-//            NimRODLookAndFeel laf = new NimRODLookAndFeel();
-//            laf.setCurrentTheme(new PraxisTheme
             LookAndFeel laf = new PraxisLookAndFeel();
             UIManager.setLookAndFeel(laf);
         } catch (UnsupportedLookAndFeelException ex) {
--- praxis.laf/src/net/neilcsmith/praxis/laf/PraxisLookAndFeel.java
+++ praxis.laf/src/net/neilcsmith/praxis/laf/PraxisLookAndFeel.java
@@ -1,56 +1,128 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 2010 Neil C Smith.
+ * Copyright 2014 Neil C Smith.
  *
  * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation; either version 2.1 of the License,
- * or (at your option) any later version.
+ * under the terms of the GNU General Public License version 3 only, as
+ * published by the Free Software Foundation.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
- * for more details.
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 3 for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
+ * You should have received a copy of the GNU General Public License version 3
  * along with this work; if not, see http://www.gnu.org/licenses/
- * 
+ *
  *
  * Please visit http://neilcsmith.net if you need additional information or
  * have any questions.
- *
  */
-
 package net.neilcsmith.praxis.laf;
 
-import com.nilo.plaf.nimrod.NimRODLookAndFeel;
+import java.awt.Color;
 import javax.swing.UIDefaults;
+import javax.swing.plaf.ColorUIResource;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 
 /**
+ * Dark-themed Nimbus l&f
  *
  * @author Neil C Smith
  */
-public class PraxisLookAndFeel extends NimRODLookAndFeel {
+public class PraxisLookAndFeel extends NimbusLookAndFeel {
+//
+
+    private final static Color DARK_GREY = new ColorUIResource(Color.decode("#121212"));
+    private final static Color MEDIUM_GREY = new ColorUIResource(Color.decode("#262626"));
+    private final static Color LIGHT_GREY = new ColorUIResource(Color.decode("#AAAAAA"));
+    private final static Color OFF_WHITE = new ColorUIResource(Color.decode("#DCDCDC"));
+    private final static Color PRIMARY = new ColorUIResource(Color.decode("#4545A1"));
+    private final static Color SECONDARY = new ColorUIResource(Color.decode("#5959B5"));
     
-    public PraxisLookAndFeel() {
-        setCurrentTheme(new PraxisTheme());
-    }
+    private final static Color BASE = new ColorUIResource(Color.decode("#010105"));
 
     @Override
-    public String getID() {
+    public String getName() {
         return "Praxis";
     }
-
-
-
+    
     @Override
-    protected void initComponentDefaults(UIDefaults table) {
-        super.initComponentDefaults(table);
-        table.put("ButtonUI", "net.neilcsmith.praxis.laf.PraxisButtonUI");
-        table.put("ToggleButtonUI", "net.neilcsmith.praxis.laf.PraxisToggleButtonUI");
+    public UIDefaults getDefaults() {
+        UIDefaults res = super.getDefaults();
+        extendDefaults(res);
+        return res;
     }
 
+    private static void swap(UIDefaults def, String key1, String key2) {
+        Object tmp = def.get(key1);
+        def.put(key1, def.get(key2));
+        def.put(key2, tmp);
+    }
+    
+    private void extendDefaults(UIDefaults res) {
 
+        // BEGIN :: Keep this section in sync with PraxisLookAndFeel
+        res.put("control", DARK_GREY);
+        res.put("info", Color.BLACK);// new Color(128,128,128) );
+        res.put("nimbusBase", BASE);
+        res.put("nimbusBlueGrey", MEDIUM_GREY);
+        res.put("nimbusDisabledText", LIGHT_GREY);
+        res.put("nimbusFocus", SECONDARY);
+        res.put("nimbusInfoBlue", MEDIUM_GREY);
+        res.put("nimbusLightBackground", Color.BLACK);
+        res.put("nimbusOrange", PRIMARY);
+        res.put("nimbusSelectedText", Color.WHITE);
+        res.put("nimbusSelectionBackground", PRIMARY);
+        res.put("text", OFF_WHITE);
+        res.put("textForeground", OFF_WHITE);
+        res.put("textText", OFF_WHITE);
+
+        //  Menus
+        SolidColorPainter primaryBG = new SolidColorPainter(PRIMARY);
+        SolidColorPainter greyBG = new SolidColorPainter(MEDIUM_GREY);
+        res.put("PopupMenu[Disabled].backgroundPainter", greyBG);
+        res.put("PopupMenu[Enabled].backgroundPainter", greyBG);
+        res.put("MenuBar:Menu[Enabled].textForeground", OFF_WHITE);
+        res.put("Menu[Enabled].textForeground", OFF_WHITE);
+        res.put("Menu[Disabled].textForeground", LIGHT_GREY);
+        res.put("MenuItem[Enabled].textForeground", OFF_WHITE);
+        res.put("MenuItem[Disabled].textForeground", LIGHT_GREY);
+        res.put("CheckBoxMenuItem[Enabled].textForeground", OFF_WHITE);
+        res.put("RadioButtonMenuItem[Enabled].textForeground", OFF_WHITE);
+        res.put("Menu[Enabled].arrowIconPainter", res.get("Menu[Enabled+Selected].arrowIconPainter"));
+        res.put("MenuBar:Menu[Selected].backgroundPainter", greyBG);
+        res.put("Menu[Enabled+Selected].backgroundPainter", primaryBG);
+        res.put("MenuItem[MouseOver].backgroundPainter", primaryBG);
+        res.put("RadioButtonMenuItem[MouseOver].backgroundPainter", primaryBG);
+        res.put("RadioButtonMenuItem[MouseOver+Selected].backgroundPainter", primaryBG);
+        res.put("CheckBoxMenuItem[MouseOver].backgroundPainter", primaryBG);
+        res.put("CheckBoxMenuItem[MouseOver+Selected].backgroundPainter", primaryBG);
+
+        res.put("Tree[Enabled].collapsedIconPainter", res.get("Tree[Enabled+Selected].collapsedIconPainter"));
+        res.put("Tree[Enabled].expandedIconPainter", res.get("Tree[Enabled+Selected].expandedIconPainter"));
+
+        res.put("Table[Enabled].textForeground", OFF_WHITE);
+        res.put("Table[Enabled+Selected].textForeground", OFF_WHITE);
+
+        res.put("TabbedPane:TabbedPaneTab[Enabled].backgroundPainter", null);
+        res.put("TabbedPane:TabbedPaneTab[Disabled].backgroundPainter", null);
+        
+        res.put("ToggleButton[Selected].backgroundPainter", res.get("Button[Default].backgroundPainter"));
+        res.put("ToggleButton[MouseOver+Selected].backgroundPainter", res.get("Button[Default+MouseOver].backgroundPainter"));
+        res.put("ToggleButton[Focused+Selected].backgroundPainter", res.get("Button[Default+Focused].backgroundPainter"));
+        res.put("ToggleButton[Focused+MouseOver+Selected].backgroundPainter", res.get("Button[Default+Focused+MouseOver].backgroundPainter"));
+        res.put("ToolBar:ToggleButton[Selected].backgroundPainter", res.get("Button[Default].backgroundPainter"));
+        res.put("ToolBar:ToggleButton[MouseOver+Selected].backgroundPainter", res.get("Button[Default+MouseOver].backgroundPainter"));
+        res.put("ToolBar:ToggleButton[Focused+Selected].backgroundPainter", res.get("Button[Default+Focused].backgroundPainter"));
+        res.put("ToolBar:ToggleButton[Focused+MouseOver+Selected].backgroundPainter", res.get("Button[Default+Focused+MouseOver].backgroundPainter"));
+
+        res.put("ComboBox.selectionBackground", new Color(PRIMARY.getRGB()));
+        
+        // END :: Keep this section in sync with PraxisLookAndFeel
+
+
+    }
 
 }
