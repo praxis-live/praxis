9aaab4d9bad12696bb6a079b18b087c56daf0597
Neil C Smith <neilcsmith.net@googlemail.com>
2014-12-04 19:20:46 +0000

    praxis.video.code - tidy up imports in VideoCodeConnector.

--- praxis.video.code/src/net/neilcsmith/praxis/video/code/VideoCodeConnector.java
+++ praxis.video.code/src/net/neilcsmith/praxis/video/code/VideoCodeConnector.java
@@ -23,8 +23,6 @@
 package net.neilcsmith.praxis.video.code;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import net.neilcsmith.praxis.code.CodeConnector;
 import net.neilcsmith.praxis.code.CodeFactory;
