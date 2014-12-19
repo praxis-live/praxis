df9d32d1de4a4e84d2b6b7fec6971b02c949801e
Neil C Smith <neilcsmith.net@googlemail.com>
2014-12-19 10:50:34 +0000

    praxis.code - add ability to read source file from JAR in AbstractComponentFactory.

--- praxis.code/src/net/neilcsmith/praxis/code/AbstractComponentFactory.java
+++ praxis.code/src/net/neilcsmith/praxis/code/AbstractComponentFactory.java
@@ -21,9 +21,16 @@
  */
 package net.neilcsmith.praxis.code;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import net.neilcsmith.praxis.core.Component;
 import net.neilcsmith.praxis.core.ComponentFactory;
 import net.neilcsmith.praxis.core.ComponentInstantiationException;
@@ -54,6 +61,7 @@ public class AbstractComponentFactory implements ComponentFactory {
         return new ComponentType[0];
     }
 
+    @Override
     public Component createComponent(ComponentType type) throws ComponentInstantiationException {
         MetaData data = componentMap.get(type);
         if (data == null) {
@@ -66,24 +74,38 @@ public class AbstractComponentFactory implements ComponentFactory {
         }
     }
 
+    @Override
     public Root createRootComponent(ComponentType type) throws ComponentInstantiationException {
         throw new IllegalArgumentException();
     }
 
+    @Override
     public ComponentFactory.MetaData<? extends Component> getMetaData(ComponentType type) {
         return componentMap.get(type);
     }
-    
+
+    @Override
     public ComponentFactory.MetaData<? extends Root> getRootMetaData(ComponentType type) {
         return null;
     }
-    
+
     protected void addComponent(Data info) {
         componentMap.put(info.factory.getComponentType(), info.toMetaData());
     }
-    
-    public static Data data(CodeFactory<?> factory) {
+
+    protected Data data(CodeFactory<?> factory) {
         return new Data(factory);
+    }
+
+    protected String source(String location) {
+        try (InputStream is = getClass().getResourceAsStream(location);
+                Scanner s = new Scanner(is, "UTF-8").useDelimiter("\\A");) {
+            return s.hasNext() ? s.next() : ""; 
+        } catch (Exception e) {
+            Logger.getLogger(AbstractComponentFactory.class.getName())
+                    .log(Level.SEVERE, "Failed to read source file", e);
+            return null;
+        }
     }
 
     private static class MetaData extends ComponentFactory.MetaData<Component> {
@@ -132,41 +154,40 @@ public class AbstractComponentFactory implements ComponentFactory {
         public Lookup getLookup() {
             return lookup == null ? super.getLookup() : lookup;
         }
-        
+
         private CodeFactory<?> getCodeFactory() {
             return factory;
         }
-        
-        
+
     }
-    
+
     public static class Data {
 
         private final CodeFactory<?> factory;
         private boolean test;
         private boolean deprecated;
         private ComponentType replacement;
-        
+
         private Data(CodeFactory<?> factory) {
             this.factory = factory;
         }
-        
+
         public Data test() {
             test = true;
             return this;
         }
-        
+
         public Data deprecated() {
             deprecated = true;
             return this;
         }
-        
+
         public Data replacement(String type) {
             replacement = ComponentType.create(type);
             deprecated = true;
             return this;
         }
-        
+
         private MetaData toMetaData() {
             return new MetaData(factory, test, deprecated, replacement, null);
         }
