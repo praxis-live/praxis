2260f8f195c9f88906e152abe26838f8bbce46d6
Neil C Smith <neilcsmith.net@googlemail.com>
2016-07-28 12:22:34 +0100

    praxis.code.services - add class caching for built-in component code (as in old code factory)

--- praxis.code.services/src/net/neilcsmith/praxis/code/services/DefaultCodeFactoryService.java
+++ praxis.code.services/src/net/neilcsmith/praxis/code/services/DefaultCodeFactoryService.java
@@ -22,6 +22,8 @@
 package net.neilcsmith.praxis.code.services;
 
 import java.util.EnumSet;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import net.neilcsmith.praxis.code.CodeCompilerService;
 import net.neilcsmith.praxis.code.CodeComponent;
 import net.neilcsmith.praxis.code.CodeComponentFactoryService;
@@ -50,8 +52,11 @@ import net.neilcsmith.praxis.logging.LogLevel;
  */
 public class DefaultCodeFactoryService extends AbstractRoot {
 
+    private final static ConcurrentMap<ClassCacheKey, Class<? extends CodeDelegate>> CODE_CACHE
+            = new ConcurrentHashMap<>();
+
     private final ComponentRegistry registry;
-    
+
     public DefaultCodeFactoryService() {
         super(EnumSet.noneOf(Caps.class));
         registry = ComponentRegistry.getInstance();
@@ -61,65 +66,77 @@ public class DefaultCodeFactoryService extends AbstractRoot {
         registerInterface(CodeContextFactoryService.class);
     }
 
-    
     private ControlAddress findCompilerService() throws Exception {
         return ControlAddress.create(findService(CodeCompilerService.class),
                 CodeCompilerService.COMPILE);
     }
-    
-    
+
     private PMap createCompilerTask(ClassBodyContext<?> cbc, LogLevel logLevel, String source) {
         return PMap.create(
                 CodeCompilerService.KEY_CLASS_BODY_CONTEXT,
-                cbc.getClass().getName(), 
+                cbc.getClass().getName(),
                 CodeCompilerService.KEY_LOG_LEVEL,
                 logLevel.asPString(),
                 CodeCompilerService.KEY_CODE,
                 source);
     }
-    
-    private CodeDelegate extractCodeDelegate(Argument response) throws Exception {
+
+    private Class<? extends CodeDelegate> extractCodeDelegateClass(Argument response) throws Exception {
         PMap data = PMap.coerce(response);
         PMap classes = PMap.coerce(data.get(CodeCompilerService.KEY_CLASSES));
         ClassLoader classLoader = new PMapClassLoader(classes, Thread.currentThread().getContextClassLoader());
-        return (CodeDelegate) classLoader.loadClass("$").newInstance();
+        return (Class<? extends CodeDelegate>) classLoader.loadClass("$");
     }
-    
+
     private void extractCompilerLog(Argument response, LogBuilder logBuilder) throws Exception {
         PMap data = PMap.coerce(response);
         PArray log = PArray.coerce(data.get(CodeCompilerService.KEY_LOG));
-        for (int i=0; i<log.getSize(); i+=2) {
-            logBuilder.log(LogLevel.valueOf(log.get(i).toString()), log.get(i+1).toString());
+        for (int i = 0; i < log.getSize(); i += 2) {
+            logBuilder.log(LogLevel.valueOf(log.get(i).toString()), log.get(i + 1).toString());
         }
     }
-    
 
     private class NewInstanceControl extends AbstractAsyncControl {
 
         @Override
         protected Call processInvoke(Call call) throws Exception {
-            CodeFactory<?> codeFactory = findCodeFactory();
-            return Call.createCall(
-                    findCompilerService(),
-                    getAddress(),
-                    call.getTimecode(),
-                    createCompilerTask(codeFactory.getClassBodyContext(),
-                            LogLevel.ERROR,
-                            codeFactory.getSourceTemplate()));
+            CodeFactory<CodeDelegate> codeFactory = findCodeFactory();
+            ClassBodyContext<?> cbc = codeFactory.getClassBodyContext();
+            String src = codeFactory.getSourceTemplate();
+            Class<? extends CodeDelegate> cls = CODE_CACHE.get(new ClassCacheKey(cbc, src));
+            if (cls != null) {
+                return Call.createReturnCall(call,
+                        PReference.wrap(createComponent(codeFactory, cls)));
+            } else {
+                return Call.createCall(
+                        findCompilerService(),
+                        getAddress(),
+                        call.getTimecode(),
+                        createCompilerTask(cbc, LogLevel.ERROR, src));
+            }
+
         }
 
         @Override
         protected Call processResponse(Call call) throws Exception {
             CodeFactory<CodeDelegate> codeFactory = findCodeFactory();
-            CodeDelegate delegate = extractCodeDelegate(call.getArgs().get(0));
+            Class<? extends CodeDelegate> cls = extractCodeDelegateClass(call.getArgs().get(0));
+            CodeDelegate delegate = cls.newInstance();
             CodeComponent<CodeDelegate> cmp = codeFactory.task().createComponent(delegate);
+            CODE_CACHE.putIfAbsent(new ClassCacheKey(codeFactory.getClassBodyContext(), codeFactory.getSourceTemplate()), cls);
             return Call.createReturnCall(getActiveCall(), PReference.wrap(cmp));
         }
-        
+
         private CodeFactory<CodeDelegate> findCodeFactory() throws Exception {
             ComponentType type = ComponentType.coerce(getActiveCall().getArgs().get(0));
             ComponentFactory cmpFactory = registry.getComponentFactory(type);
             return cmpFactory.getMetaData(type).getLookup().get(CodeFactory.class);
+        }
+
+        private CodeComponent<CodeDelegate> createComponent(
+                CodeFactory<CodeDelegate> codeFactory,
+                Class<? extends CodeDelegate> delegateClass) throws Exception {
+            return codeFactory.task().createComponent(delegateClass.newInstance());
         }
 
         @Override
@@ -136,37 +153,55 @@ public class DefaultCodeFactoryService extends AbstractRoot {
         protected Call processInvoke(Call call) throws Exception {
             CodeContextFactoryService.Task<CodeDelegate> task = findTask();
             CodeFactory<CodeDelegate> factory = task.getFactory();
+            ClassBodyContext<CodeDelegate> cbc = factory.getClassBodyContext();
             String src = task.getCode();
+            Class<? extends CodeDelegate> cls;
             if (src.trim().isEmpty()) {
                 src = factory.getSourceTemplate();
+                cls = CODE_CACHE.get(new ClassCacheKey(cbc, src));
+            } else {
+                // @TODO weak code cache for user code
+                cls = null;
             }
-            return Call.createCall(
-                    findCompilerService(),
-                    getAddress(),
-                    call.getTimecode(),
-                    createCompilerTask(factory.getClassBodyContext(),
-                            LogLevel.ERROR,
-                            src));
+            if (cls != null) {
+                LogBuilder log = new LogBuilder(task.getLogLevel());
+                CodeDelegate delegate = cls.newInstance();
+                return Call.createReturnCall(call,
+                        PReference.wrap(createContext(task, log, delegate)));
+            } else {
+                return Call.createCall(
+                        findCompilerService(),
+                        getAddress(),
+                        call.getTimecode(),
+                        createCompilerTask(cbc, LogLevel.ERROR, src));
+            }
+
         }
 
         @Override
         protected Call processResponse(Call call) throws Exception {
             CodeContextFactoryService.Task<CodeDelegate> task = findTask();
-            CodeDelegate delegate = extractCodeDelegate(call.getArgs().get(0));
+            Class<? extends CodeDelegate> cls = extractCodeDelegateClass(call.getArgs().get(0));
+            CodeDelegate delegate = cls.newInstance();
             LogBuilder log = new LogBuilder(task.getLogLevel());
             extractCompilerLog(call.getArgs().get(0), log);
+            return Call.createReturnCall(getActiveCall(),
+                    PReference.wrap(createContext(task, log, delegate)));
+        }
+
+        private CodeContextFactoryService.Task<CodeDelegate> findTask() throws Exception {
+            return (CodeContextFactoryService.Task<CodeDelegate>) PReference.coerce(getActiveCall().getArgs().get(0)).getReference();
+        }
+
+        private CodeContextFactoryService.Result<CodeDelegate> createContext(
+                CodeContextFactoryService.Task<CodeDelegate> task,
+                LogBuilder log,
+                CodeDelegate delegate) {
             CodeContext<CodeDelegate> context = task.getFactory().task()
                     .attachPrevious(task.getPrevious())
                     .attachLogging(log)
                     .createContext(delegate);
-            CodeContextFactoryService.Result<CodeDelegate> result =
-                    new CodeContextFactoryService.Result<>(context, log);
-            return Call.createReturnCall(getActiveCall(), PReference.wrap(result));
-        }
-        
-        private CodeContextFactoryService.Task<CodeDelegate> findTask() throws Exception {
-            return (CodeContextFactoryService.Task<CodeDelegate>)
-                    PReference.coerce(getActiveCall().getArgs().get(0)).getReference();
+            return new CodeContextFactoryService.Result<>(context, log);
         }
 
         @Override
